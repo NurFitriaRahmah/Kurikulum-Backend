4eaf8ccb193a702a0d1cf4a72bd51640af905f55
Fitria <nurfitria@gmail.com>
2020-11-18 09:05:56 +0700

    sprint4

---
+++ Sprint4/topik1/action.php
@@ -0,0 +1,22 @@
+<?php
+$nama = $_POST['nama_barang'];
+$harga = $_POST['harga'];
+
+echo $nama;
+echo "<br>";
+echo $harga;
+echo "<br>";
+
+$nama = $_GET['nama_kurir'];
+$nama1 = $_GET['nama_pengirim'];
+$asal = $_GET['asal_barang'];
+$tujuan = $_GET['tujuan_barang'];
+
+echo $nama;
+echo "<br>";
+echo $nama1;
+echo "<br>";
+echo $asal;
+echo "<br>";
+echo $tujuan;
+echo "<br>";
